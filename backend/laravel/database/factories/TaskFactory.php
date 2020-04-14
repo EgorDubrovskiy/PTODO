4feab640ac11fdfa67252463a13ccbfbee100f42
EgorDubrovskiy <egor.dubrovskiy.1998@mail.ru>
2020-04-14 16:04:40 +0300

    Add Factory for Tasks

---
+++ backend/laravel/database/factories/TaskFactory.php
@@ -0,0 +1,46 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\User;
+use App\Models\Task;
+use Faker\Generator as Faker;
+
+$factory->define(Task::class, function (Faker $faker) {
+    $createdAt = $faker->dateTime('01.01.2020');
+    $startedAt = $faker->dateTimeBetween(
+        (clone $createdAt)->add(new DateInterval('PT1S')),
+        (clone $createdAt)->add(new DateInterval('PT5H'))
+    );
+
+    return [
+        'created_at' => $createdAt,
+        'updated_at' => now(),
+        'deleted_at' => null,
+        'started_at' => $startedAt,
+        'done_at' => $faker->dateTimeBetween(
+            (clone $startedAt)->add(new DateInterval('PT1M')),
+            (clone $startedAt)->add(new DateInterval('PT5H'))
+        ),
+        'scheduled_at' => $startedAt->format('Y-m-d'),
+        'notify_at' => $faker->dateTimeBetween(
+            (clone $startedAt)->add(new DateInterval('PT1M')),
+            (clone $startedAt)->add(new DateInterval('PT10H'))
+        ),
+        'expected_time' => $faker->dateTimeBetween(
+            (new DateTime())->setTime(0, 5),
+            (new DateTime())->setTime(10, 0)
+        )->format('H:i:s'),
+        'text' => $faker->realText(60),
+        'parent_task_id' => null,
+        'user_id' => null,
+    ];
+});
+
+$factory->state(Task::class, 'deleted', [
+    'deleted_at' => now(),
+]);
+
+$factory->state(Task::class, 'random_user', [
+    'user_id' => User::all(['id'])->random()->id,
+]);
