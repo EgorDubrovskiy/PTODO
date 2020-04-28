defa73b48f41e9accdba0dc987d876a80d9316da
EgorDubrovskiy <egor.dubrovskiy.1998@mail.ru>
2020-04-28 12:39:36 +0300

    Add TestSeeder and refactor TestUserTableSeeder. Change UserFactory.

---
+++ backend/laravel/database/seeds/Test/TestSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Collection;
+use App\Interfaces\Services\DatabaseFactory\FactoryStateServiceInterface;
+use Illuminate\Database\Eloquent\Factory;
+use Illuminate\Database\Eloquent\FactoryBuilder;
+
+/**
+ * Class TestSeeder
+ */
+abstract class TestSeeder extends Seeder
+{
+    /**
+     * @var FactoryStateServiceInterface $stateService
+     */
+    protected $stateService;
+
+    /**
+     * @var Factory $eloquentFactory
+     */
+    protected $eloquentFactory;
+
+    /**
+     * TestSeeder constructor.
+     * @param FactoryStateServiceInterface $stateService
+     * @param Factory $eloquentFactory
+     */
+    public function __construct(FactoryStateServiceInterface $stateService, Factory $eloquentFactory)
+    {
+        $this->stateService = $stateService;
+        $this->eloquentFactory = $eloquentFactory;
+    }
+
+    /**
+     * @param string $className
+     * @param array $stateAttributes
+     * @param int $amount
+     * @return Collection
+     */
+    public function createByCustomState(string $className, array $stateAttributes, int $amount = 1): Collection
+    {
+        $state = $this->stateService->getStateByAttributes($stateAttributes);
+
+        return $this->getFactoryBuilder($className, $amount)->create($state);
+    }
+
+    /**
+     * @param string $className
+     * @param array $stateAttributes
+     * @param int $amount
+     * @return Collection
+     */
+    public function makeByCustomState(string $className, array $stateAttributes, int $amount = 1): Collection
+    {
+        $state = $this->stateService->getStateByAttributes($stateAttributes);
+
+        return $this->getFactoryBuilder($className, $amount)->make($state);
+    }
+
+    /**
+     * @param string $className
+     * @param int $amount
+     * @return FactoryBuilder
+     */
+    protected function getFactoryBuilder(string $className, int $amount): FactoryBuilder
+    {
+        return $this->eloquentFactory->of($className)->times($amount);
+    }
+}
--- backend/laravel/database/seeds/Test/User/TestUserTableSeeder.php
+++ backend/laravel/database/seeds/Test/User/TestUserTableSeeder.php
@@ -1,12 +1,11 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use App\User;
 
 /**
  * Class TestUserTableSeeder
  */
-class TestUserTableSeeder extends Seeder
+class TestUserTableSeeder extends TestSeeder
 {
     /**
      * Run the database seeds.
@@ -18,7 +17,7 @@ class TestUserTableSeeder extends Seeder
         $amountSimpleUsers = (int) config('database.seeders.test.user.amount_simple_users');
         $amountDeletedUsers = (int) config('database.seeders.test.user.amount_deleted_users');
 
-        factory(User::class, $amountSimpleUsers)->create();
-        factory(User::class, $amountDeletedUsers)->state('deleted')->create();
+        $this->getFactoryBuilder(User::class, $amountSimpleUsers)->create();
+        $this->createByCustomState(User::class, ['deleted_at'], $amountDeletedUsers);
     }
 }
