290dd6701e131cd6dbabe0da4e4c50223c9d0312
EgorDubrovskiy <egor.dubrovskiy.1998@mail.ru>
2020-04-29 08:14:18 +0300

    Add TestTaskTemplateTableSeeder to the test seeders list

--- backend/laravel/database/seeds/DatabaseSeeder.php
+++ backend/laravel/database/seeds/DatabaseSeeder.php
@@ -26,6 +26,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             TestUserTableSeeder::class,
+            TestTaskTemplateTableSeeder::class,
             TestTaskTableSeeder::class,
         ]);
     }
